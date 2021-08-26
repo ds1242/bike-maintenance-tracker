43477bb6d58426d9a01b5506ff0b676c38732d59
ds1242 <david.shaw1242@gmail.com>
2021-08-26 06:51:27 -0600

    initial navbar setup with conditional rendering

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
-// import Auth from '../../utils/auth';
+import Auth from '../../utils/auth';
 import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
+import './Nav.css';
 
 function Nav() {
-
+    function showNav() {
+        if(Auth.loggedIn()) {
+            return (
+                <li className='navActive'>
+                    <Link to={'/profile'} className='nav-link active'>
+                        <h3>Profile</h3>
+                    </Link>
+                </li>
+            )
+        } else {
+            return (
+                <li className='navActive'>
+                    <Link to={'/profile'} className='nav-link active'>
+                        <h3>Profile</h3>
+                    </Link>
+                </li>
+            )
+        }
+    }
     
     return (
         <nav className='navbar navbar-expand-sm'>
-            <div className="containerfluid" >
-                <Link to={'/'}>
-                    <Button variant="primary">Primary</Button>{' '}
+            <div className="container-fluid" >
+                <Link to={'/'} className="navbar-brand">
+                    <h1>Bike Maintenance Tracker</h1>
                 </Link>
+                <ul className="navbar-nav">
+                    {showNav()}
+                </ul>
             </div>
         </nav>
     )
