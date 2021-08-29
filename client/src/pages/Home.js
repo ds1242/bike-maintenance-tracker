a640b662c83a800fd15d648302cca8b59d8eece8
ds1242 <david.shaw1242@gmail.com>
2021-08-29 09:00:32 -0600

    setup links for signup and login on homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import Auth from '../utils/auth';
+import { Link } from 'react-router-dom';
+
+
 
 const Home = () => {
+
+    const loggedIn = Auth.loggedIn()
+
+
+    if(loggedIn) {
+        return (
+            <div className="homeContainer">
+                Logged In!
+            </div>
+        )
+    } else {
     return (
         <div className="homeContainer">
-            <h2>Home</h2>
+            <Link to={'/signup'} className='home-links'>
+                <h3>Click Here to Sign Up</h3>
+            </Link>
+            <Link to={'/login'}>
+                <h3>Already a member? Click Here to Login</h3>
+            </Link>
         </div>
     );
-
+    }
 };
 
 export default Home;
