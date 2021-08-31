f8d6611c59f320b1c51090cfb769f9b12f8f0bbd
ds1242 <david.shaw1242@gmail.com>
2021-08-30 18:30:00 -0600

    imported all the bike fields

--- client/src/pages/MyBikes.js
+++ client/src/pages/MyBikes.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { useParams } from 'react-router-dom'
-import {GET_SINGLE_USER_BIKES} from '../utils/queries';
+import { QUERY_SINGLE_BIKE } from '../utils/queries';
 // import Auth from '../utils/auth';
 
 function MyBikes() {
     const { id: bikeId } = useParams();
-    console.log(bikeId);
+    const { loading, data } = useQuery(QUERY_SINGLE_BIKE, {
+        variables: { _id: bikeId }
+    });
+
+    console.log(data);
+    const { 
+        _id,
+        make, 
+        model, 
+        year, 
+        bikeType, 
+        shockMake, 
+        shockModel, 
+        shockHours, 
+        rearDeraileurModel, 
+        rearDeraileurMake, 
+        rearDeraileurMiles,
+        frontDeraileurModel,
+        frontDeraileurMake,
+        frontDeraileurMiles,
+        forkMake,
+        forkModel,
+        forkHours,
+        chainringModel,
+        chainringMiles,
+        chainringMake,
+        chainName,
+        chainMiles,
+        cassetteModel,
+        cassetteMiles,
+        cassetteMake,
+        bikePhoto
+    } = data?.bike || {};
 
     return (
         <div>
