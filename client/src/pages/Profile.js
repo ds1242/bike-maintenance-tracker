2481581b57da538cbe825b1c6448c0775b6f288a
ds1242 <david.shaw1242@gmail.com>
2021-09-06 13:31:43 -0600

    started profile card to display user profile information

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,34 @@
 import React from 'react';
 // import Auth from '../utils/auth';
 // import { Link } from 'react-router-dom';
-// import { useQuery } from '@apollo/react-hooks';
-// import { QUERY_ME } from '../utils/queries'
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_ME } from '../utils/queries'
 
 function Profile() {
+
+    const { loading, data } = useQuery(QUERY_ME);
+
+    const me = data?.me || {};
+    console.log(me);
+    if(loading) {
+        return <h3>Loading...</h3>
+    };
+
+
     return (
-        <h2>Profile</h2>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={me.userImage}/>
+            <Card.Body>
+                <Card.Title>{me.name}</Card.Title>
+                <Card.Text>{me.userBio}</Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroupItem>Cras justo odio</ListGroupItem>
+                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+        </Card>
     )
 };
 
