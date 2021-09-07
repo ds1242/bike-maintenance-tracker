b8e51426dbbc218bdaba8bcad24b0abfbc4f9cfe
ds1242 <david.shaw1242@gmail.com>
2021-09-07 17:04:04 -0600

    setup form submit to redirect to user, form updates user information

--- client/src/pages/EditProfile.js
+++ client/src/pages/EditProfile.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_ME } from '../utils/queries'
 import { UPDATE_USER } from '../utils/mutations';
 
@@ -9,7 +9,7 @@ const EditProfile = () => {
 
     const { loading, data } = useQuery(QUERY_ME)
 
-    const { name, username, email, userBio, userImage } = data?.me || {}
+    const { _id, name, username, email, userBio, userImage } = data?.me || {}
     const [userData, setUserData] = useState({
         name: name,
         username: username,
@@ -18,22 +18,42 @@ const EditProfile = () => {
         userImage: userImage
     });
 
+    const [updateUser] = useMutation(UPDATE_USER, {
+        variables: {_id: _id}
+    })
+
     const handleChange = (event) => {
         const { name, value } = event.target;
         setUserData({
             ...userData,
             [name]: value,
         })
+    };
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            await updateUser({
+                variables: {
+                    ...userData
+                }
+            })
+            window.location.replace(`/profile`);
+        } catch(e) {
+            console.log(e);
+        }
     }
+
 
     if(loading) {
         return(
             <h2>Loading...</h2>
         )
-    }
+    };
     return (
         <Container fluid>
-            <Form>
+            <Form onSubmit={handleFormSubmit}>
                 <Form.Group className="mb-3">
                     <Form.Label>Name</Form.Label>
                     <Form.Control type="text" name="name" value={userData.name} onChange={handleChange} />
@@ -48,8 +68,13 @@ const EditProfile = () => {
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>User Bio</Form.Label>
-                    <Form.Control type="text" name="username" value={userData.userBio} onChange={handleChange} />
+                    <Form.Control type="text" name="userBio" value={userData.userBio} onChange={handleChange} />
                 </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>User Image</Form.Label>
+                    <Form.Control type="text" name="userImage" value={userData.userImage} onChange={handleChange} />
+                </Form.Group>
+                <Button variant="primary" type="submit">Submit</Button>
             </Form>
         </Container>
     )
