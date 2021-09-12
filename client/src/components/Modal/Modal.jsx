0555607b14659a3020b188b0d8747da679f205b1
ds1242 <david.shaw1242@gmail.com>
2021-09-12 14:28:24 -0600

    pull user input data and convert to a number, set to a variable

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { UPDATE_BIKE } from '../../utils/mutations';
@@ -8,39 +8,50 @@ function MyModal (props) {
     const {show, onClose, shockHours, forkHours, cassetteMiles, chainMiles, chainringMiles, frontDeraileurMiles, rearDeraileurMiles, _id} = props;
 
     const [bikeData, setBikeData] = useState({
-        shockHours: shockHours,
-        forkhours: forkHours,
-        cassetteMiles: cassetteMiles,
-        chainMiles: chainMiles,
-        chainringMiles: chainringMiles,
-        frontDeraileurMiles: frontDeraileurMiles, 
-        rearDeraileurMiles: rearDeraileurMiles
+        shockHours: Number(shockHours),
+        forkhours: Number(forkHours),
+        cassetteMiles: Number(cassetteMiles),
+        chainMiles: Number(chainMiles),
+        chainringMiles: Number(chainringMiles),
+        frontDeraileurMiles: Number(frontDeraileurMiles), 
+        rearDeraileurMiles: Number(rearDeraileurMiles)
     });
 
     const [updateBike] = useMutation(UPDATE_BIKE, {
         variables: { _id: _id }
     });
 
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        let rideTime = Number(document.getElementById('ride-time').value);
+        let rideDistance = Number(document.getElementById('ride-distance').value);
+        console.log(rideTime);
+        console.log(rideDistance);
+    }
+
     return(
         <Modal show={show} centered>
+            {console.log(bikeData)}
+                <form onSubmit={handleFormSubmit}>
             <Modal.Title>Add Ride Details</Modal.Title>
             <Modal.Body>
-                <form>
                     <Row>
                         <Col>
                             <h5>Ride Time:</h5>
-                            <input type="text" placeholder="Hours" />
+                            <input type="number" id='ride-time' placeholder="Hours rounded to nearest hour" />
                         </Col>
                         <Col>
                             <h5>Ride Distance:</h5>
-                            <input type="text" placeholder="Miles" />
+                            <input type="number" id='ride-distance' placeholder="Miles rounded to nearest full mile" />
                         </Col>
                     </Row>
-                </form>
             </Modal.Body>
             <Modal.Footer>
+                <Button type="submit" variant='primary'>Submit</Button>
                 <Button onClick={onClose}>Add Ride</Button>
             </Modal.Footer>
+                </form>
         </Modal>
 
     )
