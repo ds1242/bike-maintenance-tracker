b2e5d3f42d248a16506720b4f9f4783e0e156c0d
ds1242 <david.shaw1242@gmail.com>
2021-10-01 15:22:03 -0600

    added styling to sign up page form

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import { CREATE_USER } from '../utils/mutations';
-import { Button } from 'react-bootstrap';
+import { Button, Form, Container, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import Auth from '../utils/auth';
 
@@ -31,22 +31,34 @@ function SignUp() {
     };
 
     return (
-        <div>
-            <h2>Sign Up</h2>
-            <form onSubmit={handleFormSubmit}>
-                <input type='email' name='email' placeholder="Email" value={formState.email} onChange={handleChange} />
-                <input type='name' name='name' placeholder='Name' value={formState.name} onChange={handleChange} />
-                <input type='username' name='username' placeholder='Username' value={formState.username} onChange={handleChange} />
-                <input type='password' name='password' placeholder='Password' value={formState.password} onChange={handleChange} />
-                <Button as='input' type='submit' value='Sign Up!' />{' '}
-            </form>
+        <Container fluid="md">
+            <Row className="justify-content-md-center">
+                <Col>
+                    <h2>Sign Up</h2>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form onSubmit={handleFormSubmit}>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label>Email: </Form.Label>
+                            <Form.Control className='form-input' name='email' type='email' placeholder='Email' value={formState.email} onChange={handleChange} />
+                        </Form.Group>
+                        <input type='email' name='email' placeholder="Email"  />
+                        <input type='name' name='name' placeholder='Name' value={formState.name} onChange={handleChange} />
+                        <input type='username' name='username' placeholder='Username' value={formState.username} onChange={handleChange} />
+                        <input type='password' name='password' placeholder='Password' value={formState.password} onChange={handleChange} />
+                        <Button as='input' type='submit' value='Sign Up!' />{' '}
+                    </Form>
+                </Col>
+            </Row>
             {error ? (
                 <div>
                     <p className='error-text'>Something went wrong signing up</p>
                 </div>
             ) : null}
             <Link to={'/login'}>Click here to login if already a user</Link>
-        </div>
+        </Container>
     )
 };
 
